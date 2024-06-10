75622f7555a257ff3fb4cc7f17e3bd2d67d4839c
Evgeny <117734025+JeffreyWillims@users.noreply.github.com>
2024-06-10 10:15:05 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+example = 'Многоэтажка'                     #1
+print(example[0])
+
+example = 'Многоэтажка'                     #2
+print(example[-1])
+
+example = 'Многоэтажка'                     #3
+print(example[5:])
+
+example = 'Многоэтажка'                     #4
+print(example[::-1])
+
+example = 'Многоэтажка'                     #5
+print(example[1::2])
